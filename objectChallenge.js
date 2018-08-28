69397db82706239e198c9889f8e0fa07b551d45f
Maggie Leavell <mjleavell@gmail.com>
2018-08-27 21:16:10 -0500

    challenge 1

---
+++ objectChallenge.js
@@ -0,0 +1,73 @@
+// ********************CHALLENGE 1**********************
+// WRITE ALL YOUR PROPERTIES
+let elizabethSanger = {
+    congressionalDistrict: 5,
+    statements: [
+        {
+            statement: "lower taxes",
+            category: "Taxes"
+        },
+        {
+            statement: "jobs are necessary",
+            category: "Jobs"
+        },
+        {
+            statement: "strong infrastructure necessary",
+            category: "Infrastructure"
+        },
+        {
+            statement: "health care is necessary",
+            category: "Health care"
+        },
+        {
+            statement: "crime and enforcement is key",
+            category: "Crime and Enforcement"
+        },
+    ],
+    donationFormUrl: "www.google.com",
+    events: [
+        {
+            date: "04/29/1993",
+            title: "Maggie's birthday",
+            description: "eat all the candy"
+        },
+        {
+            date: "08/27/2018",
+            title: "US Open starts",
+            description: "all the tennis"
+        },
+    ],
+    volunteers: [
+        {
+            name: "jennifer sammons",
+            address: "500 interstate blvd",
+            email: "jen@nss.com",
+            phoneNumber: "615-342-3290",
+            availability: "9-5",
+            activities: "taking polls, cold calling"
+        },
+        {
+            name: "george washington",
+            address: "1800 Pennsylvania Ave, Washington, 23522",
+            email: "george@potus.com",
+            phoneNumber: "615-435-4367",
+            availability: "weekends",
+            activities: "taking polls, giving speeches"
+        },
+    ],
+    biography: "Kanye wishes he was as cool as me. Therefore, you have to pick me. #YEEZUS",
+    images: [
+        {
+            imageUrl: "https://01iajxoiw1-flywheel.netdna-ssl.com/wp-content/uploads/2017/04/black-labs-puppies-1024x564-40kb.jpg",
+            imageDescription: "black lab puppies",
+            type: "constituents"
+        },
+        {
+            imageUrl: "https://01iajxoiw1-flywheel.netdna-ssl.com/wp-content/uploads/2017/04/black-labs-puppies-1024x564-40kb.jpg",
+            imageDescription: "elizabeth with puppies",
+            type: "headshot"
+        },
+    ],
+    missionStatement: "Make politics great again",
+    urlRegisterVote: "www.usopen.org"
+}
